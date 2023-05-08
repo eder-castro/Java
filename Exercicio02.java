409a83e7b955bb3ee3f6116327a6896ded2deb04
eder-castro <132703429+eder-castro@users.noreply.github.com>
2023-05-08 00:56:11 -0300

    Ex 01 a 03

---
+++ Exercicio02.java
@@ -0,0 +1,9 @@
+public class Exercicio02 {
+    public static void main (String[] args){
+        int tot = 0;
+        for (int i=0; i<=1000; i++){
+            tot = tot + i;
+            System.out.println(tot);
+        }
+    }
+}
---
+++ Exercicio03.java
@@ -0,0 +1,10 @@
+public class Exercicio03 {
+    public static void main (String[] args){
+        for (int i=0; i<100; i++){
+            int mod = i%3;
+            if (mod==0){
+                System.out.println(i);
+            }
+        }
+    }    
+}
