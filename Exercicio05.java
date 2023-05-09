e4c605cf28eddee92eba547d744b34576069cdc3
eder-castro <132703429+eder-castro@users.noreply.github.com>
2023-05-08 23:40:39 -0300

    Exercicios 4, 5 e 7

---
+++ Exercicio05.java
@@ -0,0 +1,9 @@
+public class Exercicio05 {
+    public static void main(String[] args) {
+        long fatorial = 1;
+        for (long n = 1; n <= 25; n++){
+            fatorial = fatorial * n;
+            System.out.println(fatorial);
+        } 
+    }
+}
